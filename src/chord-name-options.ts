c2ef394626ee5ca3799a25510d70f1ea8e29c24d
Kip Robinson <kip.robinson@gmail.com>
2022-01-20 21:03:49 -0500

    more comments

--- src/chord-name-options.ts
+++ src/chord-name-options.ts
@@ -1,17 +1,79 @@
 
+/**
+ * Options used when forming the name of a chord. Some options also apply to the name of a note.
+ */
 export type ChordNameOptions = {
+  /**
+   * Whether to use flats. For example, if false, chord/note name could be F#, if true it is Gb.
+   * Default: `false`
+   */
   useFlats?: boolean;
+  
+  /**
+   * Symbol used to represent a major chord.
+   * Default: `maj`
+   */
   majorSymbol?: 'maj'|'M';
+  
+  /**
+   * Whether to omit the major symbol, when possible.
+   * For example, if true a C Major chord is just "C", but if false it is "Cmaj".
+   * For some chords, the major symbol cannot be omitted (for example, "Cmaj7").
+   * Default: `true`
+   */
   omitMajor?: boolean;
+  
+  /**
+   * Symbol used to represent a minor chord.
+   * Default: `m`
+   */
   minorSymbol?: 'min'|'m'|'-';
+  
+  /**
+   * Whether to omit minor chord symbol when possible.
+   * If true, a minor chord is represented by a lower-case letter.
+   * Default: `false`
+   */
   omitMinor?: boolean;
+  
+  /**
+   * Symbol to use for augmented chord.
+   * Default: `aug`
+   */
   augSymbol?: 'aug'|'+';
+  
+  /** 
+   * Symbol to used for a dimished chord.
+   * **Note:** a value of 'o' (lower-case letter O) will be changed to unicode degree symbol (`\u1D52`)
+   * Default: `dim`
+   */
   dimSymbol?: 'dim'|'o'|'\u1D52';
+  
+  /**
+   * Whether to use unicode symbols for accidentals.
+   * If true, sharp is represented by `\u266F`, flat by `\d266D`.
+   * If false, sharp is represented by `#`, flat by `b`.
+   * Default: `false`
+   */
   unicodeAccidentals?: boolean;
+  
+  /**
+   * Whether to use unicode symbol `\u00F8` for half-dimished chord.
+   * If false, it will be called a `m7b5` chord.
+   * Default: `false`
+   */
   unicodeHalfDiminished?: boolean;
+  
+  /**
+   * If true, some chord names will have HTML in the output. This only applies to 6/9 or 2/4 chords.
+   * Default: `false`
+   */
   useHtml?: boolean;
 }
 
+/**
+ * Similar to ChordNameOptions, but with all options specified, and a few extra derived options provided.
+ */
 export type SanitizedChordNameOptions = {
   useFlats: boolean;
   unicodeAccidentals: boolean;
@@ -28,6 +90,10 @@ export type SanitizedChordNameOptions = {
   useHtml: boolean;
 }
 
+/**
+ * Takes chord names options parameter and sanitizes them to ensure that all options are
+ * specified or defaulted.
+ */
 export const sanitizeChordNameOptions = (options?: ChordNameOptions): SanitizedChordNameOptions => {
   if('object' !== typeof options || options === null || options === undefined)
     options = {};
