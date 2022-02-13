94a025ad30921d21b82aa4c1f25aef2c0e7343b3
Kip Robinson <kip.robinson@gmail.com>
2022-02-12 23:02:52 -0500

    create dist dir, create index file with all types

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {default as Note} from './note';
+export {default as Pitch} from './pitch';
+export {default as Chord, IntervalName, Interval, ChordNameInfo} from './chord';
+export {ChordNameOptions, SanitizedChordNameOptions, sanitizeChordNameOptions} from './chord-name-options';
